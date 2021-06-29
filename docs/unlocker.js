575d776d8d1ae92c64a0a12339adcc1eb3dc805a
kriogenic <389938+kriogenic@users.noreply.github.com>
2021-06-30 07:07:06 +1000

    Update unlocker.js

--- docs/unlocker.js
+++ docs/unlocker.js
@@ -8,7 +8,7 @@ oReq.onload = function (oEvent) {
   if (arrayBuffer) {
     var byteArray = new Uint8Array(arrayBuffer);
     for (var i = 0; i < byteArray.byteLength; i++) {
-      console.log("" + byteArray[i])
+      console.log("Byte at: " + i + " is " + byteArray[i])
       // do something with each byte in the array
     }
   }
