c896cd953b86da20947d22e1cb21b2a639905f34
kriogenic <389938+kriogenic@users.noreply.github.com>
2021-06-30 07:04:22 +1000

    Update unlocker.js

--- docs/unlocker.js
+++ docs/unlocker.js
@@ -1,1 +1,17 @@
 
+var oReq = new XMLHttpRequest();
+oReq.open("GET", "./files/unlock", true);
+oReq.responseType = "arraybuffer";
+
+oReq.onload = function (oEvent) {
+  var arrayBuffer = oReq.response; // Note: not oReq.responseText
+  if (arrayBuffer) {
+    var byteArray = new Uint8Array(arrayBuffer);
+    for (var i = 0; i < byteArray.byteLength; i++) {
+      console.log("" + byteArray[i])
+      // do something with each byte in the array
+    }
+  }
+};
+
+oReq.send(null);
