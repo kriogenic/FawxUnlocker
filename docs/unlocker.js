67554e079716ae7314f6e78a0c8f2e5929306790
kriogenic <389938+kriogenic@users.noreply.github.com>
2021-06-30 11:07:18 +1000

    Update unlocker.js

--- docs/unlocker.js
+++ docs/unlocker.js
@@ -18,8 +18,8 @@ function loadFile(){
       console.log(bytestring);
       
       
-      //var steam = document.getElementById("steamid").value;
-	   var steam = "76561198046369419";
+      var steam = document.getElementById("steamid").value;
+	   
 	var id = BigInt(steam);
 	var id2 = bnToBuf(id);
 	id2.reverse();
